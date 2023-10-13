54f021367650799b54e1a43390ea8eb9cbc08ac1
FAHADPN <fahadpuzhakkaraillath@gmail.com>
2023-10-13 22:09:11 +0530

    Added Railway Database to settings.py

--- Blog_me/settings.py
+++ Blog_me/settings.py
@@ -74,11 +74,16 @@ WSGI_APPLICATION = "Blog_me.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER' : 'postgres',
+        'PASSWORD': 'W47fDAA6evPuztXxboHG',
+        'HOST': 'containers-us-west-144.railway.app',
+        'PORT': '6821'
     }
 }
+
 
 
 # Password validation
